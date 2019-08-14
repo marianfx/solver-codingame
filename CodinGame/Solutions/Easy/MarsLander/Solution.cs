9f76b04aa44428e0f4887fc1599d9946977e5610
Marian Focsa <marian.focsa@kireygroup.com>
2019-08-14 15:49:10 +0300

    Added 'Mars Lander'

---
+++ CodinGame/Solutions/Easy/MarsLander/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions.Easy.MarsLander
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs;
+            int surfaceN = int.Parse(Console.ReadLine()); // the number of points used to draw the surface of Mars.
+            for (int i = 0; i < surfaceN; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int landX = int.Parse(inputs[0]); // X coordinate of a surface point. (0 to 6999)
+                int landY = int.Parse(inputs[1]); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+            }
+
+            // game loop
+            while (true)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int X = int.Parse(inputs[0]);
+                int Y = int.Parse(inputs[1]);
+                int hSpeed = int.Parse(inputs[2]); // the horizontal speed (in m/s), can be negative.
+                int vSpeed = int.Parse(inputs[3]); // the vertical speed (in m/s), can be negative.
+                int fuel = int.Parse(inputs[4]); // the quantity of remaining fuel in liters.
+                int rotate = int.Parse(inputs[5]); // the rotation angle in degrees (-90 to 90).
+                int power = int.Parse(inputs[6]); // the thrust power (0 to 4).
+
+                // Write an action using Console.WriteLine()
+                if (power <= 3 && vSpeed <= 36)
+                    power++;
+                else
+                if (vSpeed >= 36)
+                    power++;
+                else
+                    if (power == 4)
+                    power--;
+
+                Console.WriteLine(String.Format("0 {0}", power));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Below you can see a table with all the details.
 |       Name        |  Subject   |                                   URL                                   | Difficulty | Score | Date  |
 | :---------------: | :--------: | :---------------------------------------------------------------------: | :--------: | :---: | :---: |
 | The Power of Thor | Conditions | [here](https://www.codingame.com/training/easy/power-of-thor-episode-1) |    Easy    | 100%  | 2017  |
-| The Descent | Loops | [here](https://www.codingame.com/training/easy/the-descent) |    Easy    | 100%  | 2017  |
+|    The Descent    |   Loops    |       [here](https://www.codingame.com/training/easy/the-descent)       |    Easy    | 100%  | 2017  |
+|    Mars Lander    | Conditions |  [here](https://www.codingame.com/training/easy/mars-lander-episode-1)  |    Easy    | 100%  | 2017  |
